7433660e58a052129d2741592f8a5cdd9db0d68c
Yusuf Oluwadamiloju <Damiloju@users.noreply.github.com>
2019-07-14 23:22:12 +0100

    Update index.js
    first clean draft

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const v = (digits) => {
+  digits = digits.map((el, index) => {
+    if ((index + 1) % 2 === 0) {
+      result = el * 2;
 
-const validate = (digits) => {
-        
-digits = digits.map((el,index) => {
-        	if((index + 1) % 2 === 0){
-		  				result = el * 2;
-          		if (result > 9) {
-              result = result - 9;
-           }
-            
-           el = result;
-          }
-          return el;
-        }); 
-        
-        const total = digits.reduce((c,a) => {
-        	return c+a;
-        });
-        
-        return total % 10;
+      if (result > 9) {
+        result = result - 9;
+      }
+
+      el = result;
+    }
+    return el;
+  });
+
+  const total = digits.reduce((c, a) => {
+    return c + a;
+  });
+
+  return !(!!(total % 10));
 };
-console.log(v([7,9,9,2,7,3,9,8,7,1,3]));;
+
+console.log(v([7, 9, 9, 2, 7, 3, 9, 8, 7, 1, 3]));
